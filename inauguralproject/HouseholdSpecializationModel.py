cc04151d23746349dce463b4db27994ac2dc7761
drx642 <drx642@alumni.ku.dk>
2023-03-20 10:48:43 +0100

    småændringer

--- inauguralproject/HouseholdSpecializationModel.py
+++ inauguralproject/HouseholdSpecializationModel.py
@@ -4,7 +4,7 @@ from types import SimpleNamespace
 import numpy as np
 from scipy import optimize
 
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 
 class HouseholdSpecializationModelClass:
