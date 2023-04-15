6c66a94adf1254eed044fd5d46b77962bd15df57
drx642 <125153695+drx642@users.noreply.github.com>
2023-04-15 09:25:12 +0200

    Add files via upload
    manuelt lagt op

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,16 +1,33 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import pandas as pd
+import pydst
+dst = pydst.Dst(lang='da')
 
-    Args:
-        df (pd.DataFrame): pandas dataframe 
+#creates dictionary for data
+columns_dict = {}
+columns_dict['AGEBYGROUP'] = 'agegroup'
+columns_dict['MEDICINTYPE'] = 'medicinetype'
+columns_dict['TID'] = 'year'
+columns_dict['INDHOLD'] = 'count'
+columns_dict['Bnøgle'] = 'unit'
 
-    Returns:
-        df (pd.DataFrame): pandas dataframe
+var_dict = {} # var is for variable
+var_dict['N05 Psycholeptica'] = '5'
+var_dict['N06 Psychoanaleptica'] = '6'
 
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
+unit_dict = {}
+unit_dict['Personer'] = 'person'
+unit_dict['Indløste recepter'] = 'recepter'
+
+#import data on use of medicine from dst
+Medicin4_true = dst.get_data(table_id = 'MEDICIN4', variables={'TID':['*'], 'AGEBYGROUP':['*'], 
+'MEDICINTYPE':['*']})
+
+#removes all medicinetypes that isn't Psycholeptica (downer used for anxiety and OCD)
+
+
+#drops unnecessary colums
+Medicine_drop = Medicine.drop('BNØGLE',axis=1)
+Medicine_clean = Medicine_drop.drop('KØN',axis=1)
